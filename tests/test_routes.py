da4ec2d5cdb31169621cec3c9cf3c40d840adcc2
“mangamngo” <mngomezulumwork@gmail.com>
2024-06-20 05:35:28 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,6 +14,7 @@ from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
 
+
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
@@ -187,3 +188,10 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+        
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the Cross Origin Resource Sharing header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
